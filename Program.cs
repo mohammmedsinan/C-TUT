99023f2a3f762fa0e85f6dedf98dc3561ce83a80
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2022-08-26 11:45:27 +0300

    Variables types

--- Program.cs
+++ Program.cs
@@ -4,8 +4,24 @@ namespace CTUT
 {
 class Program   
 {
-    static void Main(String[] args) {
-        Console.WriteLine("Hello WOrld!");
+    static void Main(String[] args) 
+    {
+        /***********************************************/
+        //[ dec + init ]
+        int X; //declaration
+        X = 9; //initialization
+        int Y = 9; // declaration + initialization
+        /***********************************************/
+        //[ variables types ]
+        int age = 10; //whole integer
+        double height = 120.34; //decimal Number
+        bool alive = true; //two values true , false
+        char symbol = '@'; //takes just one character
+        string name = "Mohammed"; //it takes name value
+        float atom = 12.444f; //float takes more than 2 numbers after the dot
+
+        Console.WriteLine($"this is x value {X} , and this is y value {Y}");
+        Console.ReadKey();
     }
 }    
 }
